1a784014a847a8844570349172259e9065ee73f1
bolaxaaa <henriquelanznaster@gmail.com>
2019-04-08 00:06:33 -0300

    Isso pode facilitar as coisas
    Mais descrição leia o arquivo

--- Trabalho Log/exemplo de struct em py.py
+++ Trabalho Log/exemplo de struct em py.py
@@ -1,5 +1,6 @@
 #AQUI VOCÊ CHAMA ESSE CONSTRUTOR
 from dataclasses import dataclass
+
 #USANDO O CONTRUTOR VOCÊ NÃO PRECISA MAIS FAZER TODOS AQUELES SELF.
 @dataclass
 class Point:
@@ -10,4 +11,5 @@ class Point:
 p = Point(1.5, 2.5)
 
 print(p)  # Point(x=1.5, y=2.5, z=0.0)
+
 #NOTE ESSE MODO ASSIM E MUTAVEL
